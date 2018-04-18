b41c0cff0775f2e8318f4d840b6e4a6904119549
Eric Adamski <er.adamski@gmail.com>
2018-04-17 20:59:39 -0400

    Trying to get all the data first

--- src/helpers/get.js
+++ src/helpers/get.js
@@ -4,7 +4,7 @@ const url = require('url');
 const { merge } = require('rxjs/observable/merge');
 const { fromEvent } = require('rxjs/observable/fromEvent');
 const { bindCallback } = require('rxjs/observable/bindCallback');
-const { switchMap } = require('rxjs/operators');
+const { switchMap, mapTo, tap } = require('rxjs/operators');
 
 const bufferToJson = require('./buffer-to-json');
 
@@ -13,13 +13,19 @@ const _get = bindCallback(https.get);
 module.exports = function get(uri) {
   const { hostname, path } = url.parse(uri);
 
+  let buffer;
+
   return _get({
     hostname,
     path,
     headers: { 'User-Agent': 'commit-with-cli' }
   }).pipe(
+    tap(r => fromEvent(r, 'data').subscribe(chunk => (buffer += chunk))),
     switchMap(r =>
-      merge(fromEvent(r, 'data').pipe(bufferToJson), fromEvent(r, 'error'))
+      merge(
+        fromEvent(r, 'end').pipe(mapTo(buffer), bufferToJson),
+        fromEvent(r, 'error')
+      )
     )
   );
 };
