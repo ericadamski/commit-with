9cf16d8eae7c9e8a15bfde475314811e9ba71bb6
Eric Adamski <er.adamski@gmail.com>
2018-04-17 20:50:54 -0400

    Update code

--- src/helpers/buffer-to-json.js
+++ src/helpers/buffer-to-json.js
@@ -1,8 +1,9 @@
 const { of } = require('rxjs/observable/of');
-const { map, catchError } = require('rxjs/operators');
+const { map, catchError, tap } = require('rxjs/operators');
 
 module.exports = function bufferToJson(source$) {
   return source$.pipe(
+    tap(console.log),
     map(buffer => JSON.parse(buffer.toString())),
     catchError(() => of({}))
   );
