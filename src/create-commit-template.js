95066f586a3705389694743b3a301c6083b2d3ba
Eric Adamski <er.adamski@gmail.com>
2018-04-18 00:18:24 -0400

    Actually add the files

---
+++ src/create-commit-template.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const dedent = require('dedent');
+const { of } = require('rxjs/observable/of');
+const { bindNodeCallback } = require('rxjs/observable/bindNodeCallback');
+const { catchError, switchMap, mapTo, map } = require('rxjs/operators');
+
+const { TEMPLATE_ROOT } = require('./constants');
+
+const access = bindNodeCallback(fs.access);
+const mkdir = bindNodeCallback(fs.mkdir);
+const writeFile = bindNodeCallback(fs.writeFile);
+
+function getTemplatePath(login) {
+  return path.join(TEMPLATE_ROOT, `.${login}`);
+}
+
+function coauthor({ name, email }) {
+  return dedent`
+    # Commiting with commit-with 🤗
+
+    Co-Authored-By: ${name} <${email}>
+  `;
+}
+
+module.exports = {
+  createCommitTemplate({ login, name, email }) {
+    return access(TEMPLATE_ROOT).pipe(
+      catchError(() => mkdir(TEMPLATE_ROOT)),
+      switchMap(() => {
+        const p = getTemplatePath(login);
+
+        return writeFile(p, coauthor({ name, email })).pipe(mapTo(p));
+      })
+    );
+  },
+  getCommitTemplateFor(login) {
+    const p = getTemplatePath(login);
+
+    return access(p).pipe(map(() => p), catchError(() => of(false)));
+  }
+};
